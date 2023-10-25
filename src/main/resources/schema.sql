155522df502c42f6632120de884a3aca35951710
Egor Safonov <safon0306@gmail.com>
2023-10-25 21:17:32 +0300

    Re-config CI/CD-fix: - deploy to railway

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS board_game CASCADE;
 DROP SEQUENCE IF EXISTS global_seq;
 CREATE SEQUENCE global_seq START WITH 1;
-CREATE TABLE boardGame(
+CREATE TABLE board_game(
     id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
     name        VARCHAR         NOT NULL,
     publisher   VARCHAR         NOT NULL,
